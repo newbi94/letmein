8fb7e46ae0b7505c2b7afea2874516a364a92fea
newbi94 <newbi94@gmail.com>
2024-02-29 09:53:51 +0900

    test
    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>hello</Text>
+      <Text>fuxx test</Text>
     </View>
   );
 }

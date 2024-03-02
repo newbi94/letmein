c438a889fe2a1fb1253494bf7730ada4b1a50a1d
newbi94 <newbi94@gmail.com>
2024-03-02 18:32:03 +0900

    24.03.02
    base

--- screens/Course.js
+++ screens/Course.js
@@ -4,19 +4,32 @@ import styled from "styled-components/native";
 const Container = styled.View`
   align-items: center;
 `;
-
+const TitleContainer = styled.View`
+  border-width: 1px;
+  flex-direction: row;
+`;
 const Title = styled.Text`
   font-size: 28px;
   font-weight: 600;
 `;
 
-const SerachContainer = styled.View``;
+const SerachContainer = styled.View`
+  border-width: 1px;
+  margin-top: 20px;
+  flex-direction: row;
+  padding: 5px 50px;
+`;
+const SearchInput = styled.TextInput``;
 
 const Course = () => {
   return (
     <Container>
-      <Title>Show your Course !</Title>
-      <SerachContainer></SerachContainer>
+      <TitleContainer>
+        <Title>Show your Course !</Title>
+      </TitleContainer>
+      <SerachContainer>
+        <SearchInput placeholder="what is your plan?..." />
+      </SerachContainer>
     </Container>
   );
 };

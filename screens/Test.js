da68595c97de19c667dec49c0bb0a3efe9ae256a
newbi94 <newbi94@gmail.com>
2024-03-26 16:26:55 +0900

    24.03.26
    fuxxxxxx
    npm i @react-native-commnity/geolocation,
    VS
    npm add react-native-geolocation-service,
    
    npm i react-native-permissions(ing)

--- screens/Test.js
+++ screens/Test.js
@@ -9,6 +9,9 @@ import NaverMapView, {
 } from "react-native-nmap";
 import Geolocation from "react-native-geolocation-service";
 function Test() {
+  const P0 = { latitude: 37.564362, longitude: 126.977011 };
+  const P1 = { latitude: 37.565051, longitude: 126.978567 };
+  const P2 = { latitude: 37.565383, longitude: 126.976292 };
   const [currentLocation, setCurrentLocation] = useState(null);
 
   useEffect(() => {
@@ -22,7 +25,7 @@ function Test() {
 
   useEffect(() => {
     // 위치 업데이트 설정
-    const watchId = Geolocation.watchPosition(
+    const watchId = Geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
         // currentLocation에 위도, 경도 저장
@@ -34,7 +37,7 @@ function Test() {
       {
         enableHighAccuracy: true, // 배터리를 더 소모하여 보다 정확한 위치 추적
         timeout: 20000,
-        maximumAge: 1000, // 한 번 찾은 위치 정보를 해당 초만큼 캐싱
+        maximumAge: 10000, // 한 번 찾은 위치 정보를 해당 초만큼 캐싱
         distanceFilter: 1,
       }
     );
@@ -44,19 +47,47 @@ function Test() {
     };
   }, []);
 
-  if (currentLocation === null) {
-    return <View />;
-  }
-
   return (
     <NaverMapView
       style={{ width: "100%", height: "100%" }}
       showsMyLocationButton={true}
-      center={{ ...currentLocation, zoom: 16 }}
+      center={{ latitude: 37.1, longitude: 126.7, zoom: 16 }}
       onTouch={(e) => console.warn("onTouch", JSON.stringify(e.nativeEvent))}
       onCameraChange={(e) => console.warn("onCameraChange", JSON.stringify(e))}
       onMapClick={(e) => console.warn("onMapClick", JSON.stringify(e))}
-    ></NaverMapView>
+    >
+      <Marker coordinate={P0} onClick={() => console.warn("onClick! p0")} />
+      <Marker
+        coordinate={P1}
+        pinColor="blue"
+        onClick={() => console.warn("onClick! p1")}
+      />
+      <Marker
+        coordinate={P2}
+        pinColor="red"
+        onClick={() => console.warn("onClick! p2")}
+      />
+      <Path
+        coordinates={[P0, P1]}
+        onClick={() => console.warn("onClick! path")}
+        width={10}
+      />
+      <Polyline
+        coordinates={[P1, P2]}
+        onClick={() => console.warn("onClick! polyline")}
+      />
+      <Circle
+        coordinate={P0}
+        color={"rgba(255,0,0,0.3)"}
+        radius={200}
+        onClick={() => console.warn("onClick! circle")}
+      />
+      <Polygon
+        coordinates={[P0, P1, P2]}
+        color={`rgba(0, 0, 0, 0.5)`}
+        onClick={() => console.warn("onClick! polygon")}
+      />
+    </NaverMapView>
   );
 }
 
